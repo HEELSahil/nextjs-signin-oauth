281da54bea91e3b60a090480f00c6e1129b14ae6
Kotian, Sahil Udaya <skotian@iu.edu>
2023-06-18 20:35:21 +0530

    updated path

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 
-import Nav from '@/components/Nav';
-import Provider from '@/components/Provider';
-import '@/styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+import '@styles/globals.css';
 
 
 export const metadata = {
